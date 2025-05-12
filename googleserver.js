2ae6eaf247afc532866a760ef4bdf0947a9f70c8
sarthak123kharola <sarthakkharola001@gmail.com>
2025-05-12 20:34:58 +0530

    d

--- googleserver.js
+++ googleserver.js
@@ -80,7 +80,7 @@ app.post('/mapping', (req, res) => {
 });
 
 // Server startup
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Server started on http://localhost:${PORT}`);
 });
